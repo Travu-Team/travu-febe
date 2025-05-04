9818e52c9bc1b038dc473e9ea1b0ede50800c636
luthfi <putrialuthfi@gmail.com>
2025-05-04 20:52:33 +0700

    edit halaman about

--- frontend/my-react-app/src/pages/About.jsx
+++ frontend/my-react-app/src/pages/About.jsx
@@ -5,18 +5,18 @@ const About = () => {
   return (
     <>
     <Navbar />
-    <div className="border-2 w-full min-h-screen bg-[#F2F2F2] flex flex-col items-center justify-center">
-      <div className="w-[1104px] flex flex-col items-center gap-8">
-        <div className="text-center text-[#3A59D1] text-3xl font-semibold">
+    <div className="w-full min-h-screen bg-[#F2F2F2] px-20 py-10">
+      <div className="w-[1104px] flex flex-col gap-8 px-20">
+        <div className="text-[#3A59D1] text-3xl font-semibold text-center">
           Tentang Kami
         </div>
         <div className="w-full">
         <div className="relative max-w-[956px] h-[449px] rounded-[15px] shadow-md overflow-hidden bg-cover bg-center about-image">
             <div className="absolute left-12 top-[169px] w-[430px] flex flex-col gap-3 text-white">
-              <h2 className="text-xl font-bold">Liburan Sesuai Mood? Bisa Banget!</h2>
+              <h2 className="text-2xl font-bold">Liburan Sesuai Mood? Bisa Banget!</h2>
               <p className="text-sm font-semibold w-[369px]">
-                Lagi pengen adventure? Atau healing tipis-tipis? Sistem rekomendasi kami siap jadi
-                temen setia cari destinasi yang sesuai vibe kamu. Gak perlu repot mikir, tinggal pilih aja!
+                Lagi pengen adventure? Atau healing tipis-tipis? <br />Sistem rekomendasi kami siap jadi
+                temen setia cari destinasi yang sesuai vibe kamu. <br />Gak perlu repot mikir, tinggal pilih aja!
               </p>
             </div>
           </div>
