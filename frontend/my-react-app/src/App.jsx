ee101858a8abc7d9ca6bcd2bf0819f5c69858c1e
AriscaPramalistyo05 <listyoblaze.uzu.com>
2025-05-04 16:05:30 +0700

    Buat Navbar&Footer

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "./register/RegisterPage";
 import LoginPage from "./login/LoginPage";
+import Home from './pages/Home'
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RegisterPage />} /> {/* Default halaman */}
+        <Route path="/" element={<Home />} /> {/* Default halaman */}
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<h1>404: Not Found</h1>} />
       </Routes>
     </Router>
   );
